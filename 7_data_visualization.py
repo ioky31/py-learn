b996b1ac211925d1dfcd6816c35eaf3fc5ec19e0
ioky31 <60280916+ioky31@users.noreply.github.com>
2021-10-30 16:26:54 +0800

    Update 7_data_visualization.py

--- 7_data_visualization.py
+++ 7_data_visualization.py
@@ -1,5 +1,5 @@
 #数据可视化
 import matplotlib.pyplot as plt
-plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
+plt.imshow(digits.images[-1], cmap=plt.cm.gray)
 # plt.show()
 # digits.images.shape: (1797, 8, 8)
