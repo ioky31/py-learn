cd533ab1573d350004542a9433bd1823d0c08a6a
ioky31 <60280916+ioky31@users.noreply.github.com>
2021-11-03 16:37:49 +0800

    Update 7_data_visualization.py

--- 7_data_visualization.py
+++ 7_data_visualization.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 plt.imshow(digits.images[-1], cmap=plt.cm.gray)
 # plt.show()
 # digits.images.shape: (1797, 8, 8)
+
+#plt
+import matplotlib.pyplot as plt
+plt.plot(alphas_log, coefs_T[row, :], ls="-", lw=2, label="plot figure")
