421db0ed2076580d5a49be7f154bbd7f526cca8f
ioky31 <60280916+ioky31@users.noreply.github.com>
2021-11-25 14:40:31 +0800

    Update 7_data_visualization.py

--- 7_data_visualization.py
+++ 7_data_visualization.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 plt.imshow(digits.images[-1], cmap=plt.cm.gray)
 # plt.show()
 # digits.images.shape: (1797, 8, 8)
-
+# 散点图
+plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data)
+plt.show()
 #plt
 import matplotlib.pyplot as plt
 plt.plot(alphas_log, coefs_T[row, :], ls="-", lw=2, label="plot figure")
