ef43a25c2471038a0a1ef309dce9b088a92ecfdd
ioky31 <60280916+ioky31@users.noreply.github.com>
2021-12-03 16:08:34 +0800

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -5,5 +5,5 @@ writer = pd.ExcelWriter('p_value.xlsx')
 df.to_excel(writer, index=False, encoding='utf-8', sheet_name='Sheet1')
 writer.save()
 
-%%%%%%%%%%%%%%%%%%%%%%% 保存为excel %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%% dataframe拼接 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 X_data = pd.concat([X_train, X_test], axis=0, ignore_index=True)  # 将df2数据与df1合并
