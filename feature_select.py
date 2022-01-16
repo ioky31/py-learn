4e7c973f911c8ce39d53319b659bd4c5016c33e3
ioky31 <60280916+ioky31@users.noreply.github.com>
2022-01-16 13:19:43 +0800

    Update feature_select.py

--- feature_select.py
+++ feature_select.py
@@ -18,3 +18,23 @@ from sklearn.feature_selection import RFE
 
 rfe = RFE(lr, n_features_to_select=1)
 rfe.fit(X_data, y_data)
+
+# 根据交叉验证rfe筛选特征
+def my_rfecv(X_train, y_train, need_print=False, need_draw=False):
+    svc = SVC(kernel="linear")
+    rfecv = RFECV(estimator=svc, step=1, cv=StratifiedKFold(2), scoring='accuracy')
+    rfecv.fit(X_train, y_train)
+
+    if need_print:
+        print("Optimal number of features : %d" % rfecv.n_features_)
+        print("Ranking of features : %s" % rfecv.ranking_)
+        print(rfecv.grid_scores_)
+
+    if need_draw:
+        # Plot number of features VS. cross-validation scores
+        plt.figure()
+        plt.xlabel("Number of features selected")
+        plt.ylabel("Cross validation score (nb of correct classifications)")
+        plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+        plt.show()
+    return rfecv.transform(X_train)
