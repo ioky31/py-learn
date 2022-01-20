42596cc306c6c76c8ad3f907c87e2fcf74fa10eb
ioky31 <60280916+ioky31@users.noreply.github.com>
2022-01-20 21:58:56 +0800

    Update 2_form_process.py

--- 2_form_process.py
+++ 2_form_process.py
@@ -4,4 +4,4 @@ data = data.reshape(data.shape[0], -1)
 
 #划分为训练集和测试集数据(test_size比例， random_state随机种子， stratify保持分布)
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.3, random_state=0, stratify=iris_y)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=0, stratify=iris_y)
